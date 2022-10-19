9be1b6b796a7b003698f3acfd37bc011add203fc
nelson frank <nelsonfr78@outlook.com>
2022-10-19 22:44:07 +0300

    feat: Add fallback route and error page #8
    - Using path="*"" means "match anything", so this route acts like a
    catch-all for URLs that we don't have explicit routes for.
    
    Closes #8

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="chats" element={<Chats />} />
+      <Route path="*" element={<ErrorScreen />} />
     </Routes>
   );
 }
